3f138c17eefe1562ee44e7aea17e73085386f36c
vanshika singh <vanshikajksingh1405@gmail.com>
2024-10-25 01:04:00 +0530

    Create exp15c.cpp

---
+++ exp15c.cpp
@@ -0,0 +1,23 @@
+//Vanshika Singh
+//23070123148
+
+#include <iostream>
+#include <string>
+using namespace std;
+void rev(char *str)
+{
+    if (*str)
+    {
+        rev(str+1);
+        cout<<("%c",*str);
+
+    }
+
+}
+int main()
+{
+    char s[50];
+    cout<<"Enter a string: ";
+    cin>>s;
+    rev(s);
+}
