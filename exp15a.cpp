4eccf9a0870ff54d5253046f5693626c97aa5bed
vanshika singh <vanshikajksingh1405@gmail.com>
2024-10-25 01:00:11 +0530

    Create exp15a.cpp

---
+++ exp15a.cpp
@@ -0,0 +1,25 @@
+//Vanshika Singh
+//23070123148
+
+//factorial
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    if (n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n*fact(n-1));
+    }
+}
+int main()
+{
+    int x,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    x= fact(n);
+    cout<<n<<"!: "<<x;
+}
